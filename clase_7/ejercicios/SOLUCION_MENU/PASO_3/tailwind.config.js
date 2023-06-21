12b4d81a422c9ce1a4a33fd9f77b8c206eee8067
DulceRosario <dulce.hererra@atmosfera.unam.mx>
2023-06-20 19:18:14 -0600

    clase7

---
+++ clase_7/ejercicios/SOLUCION_MENU/PASO_3/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        'clay': {
+          '50': '#f5f5f1',
+          '100': '#e6e4db',
+          '200': '#cfcab9',
+          '300': '#b3aa91',
+          '400': '#9d9072',
+          '500': '#928467',
+          '600': '#796a55',
+          '700': '#625546',
+          '800': '#55483e',
+          '900': '#4a4039',
+          '950': '#2a221e',
+        },
+      },
+      fontFamily:{
+
+        'domine': ['Domine','serif']
+      }
+    }
+  },
+  plugins: [],
+}
+
