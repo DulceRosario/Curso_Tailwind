30ec074a7d7a3aa4d76254c9fefdb3fea717e99d
DulceRosario <dulce.hererra@atmosfera.unam.mx>
2023-06-27 22:00:52 -0600

    ejercicios clase 7 y practica 8

---
+++ clase_8/ejercicios/vite/vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'; 
+
+</script>
+
+<template>
+
+  <div class=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mx-5">
+  <Card msg="Card 1" />
+  <Card msg="Card 2" />
+  <Card msg="Card 3" />
+  <Card msg="Card 4" />
+
+</div>
+</template>
+
+<style></style>
